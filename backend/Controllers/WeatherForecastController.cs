8259a55bf129df365e21aa8aac617c574abfc534
Martin Hanák <martinhanak97@gmail.com>
2024-01-14 20:04:44 +0100

    Initial message

---
+++ backend/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using backend.Data;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly BackendDbContext _context;
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, BackendDbContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public async Task<IEnumerable<TestProduct>> GetProduct(int id)
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
---
+++ backend/Data/BackendDbContext.cs
@@ -0,0 +1,40 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace backend.Data
+{
+    public class BackendDbContext : DbContext
+    {
+        public BackendDbContext(DbContextOptions<BackendDbContext> options) : base(options)
+        {
+            var dbCreater = Database.GetService<IRelationalDatabaseCreator>();
+
+            if (dbCreater != null)
+            {
+                if (!dbCreater.CanConnect())
+                {
+                    dbCreater.Create();
+                }
+
+                if (!dbCreater.HasTables())
+                {
+                    dbCreater.CreateTables();
+                }
+            }
+        }
+
+        public DbSet<TestProduct> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<TestProduct>().HasData(
+                new TestProduct() { Id = 1, Name = "Apple iPad", Price = 1000 },
+                new TestProduct() { Id = 2, Name = "Samsung Smart TV", Price = 1500 },
+                new TestProduct() { Id = 3, Name = "Nokia 130", Price = 1200 }
+            );
+        }
+    }
+}
